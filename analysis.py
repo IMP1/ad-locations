4ac0cdfbfe29ec14682baa80a9d0f08a07a58491
IMP1 <huw_taylor@hotmail.co.uk>
2020-05-11 22:02:27 +0100

    Use next instead of flag for first row

--- analysis.py
+++ analysis.py
@@ -8,11 +8,9 @@ advert_data_headers = []
 advert_data = []
 with open("adverts_2020-05-04.csv", newline="") as csv_file:
     csv_reader = csv.reader(csv_file)
+    advert_data_headers = next(csv_reader)
     for row in csv_reader:
-        if not advert_data_headers:
-            advert_data_headers = row
-        else:
-            advert_data.append(row)
+        advert_data.append(row)
 
 
 leeds_adverts = [x for x in advert_data if "Leeds" in x[2]]
